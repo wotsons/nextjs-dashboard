ac2c558f7457bea622611c4342cef76916bc9e4c
Wotson Sula <wotson.sula@smartnx.com>
2026-02-23 23:19:26 -0300

    feat: implement invoice creation and update forms with action handlers

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -12,8 +12,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
     if (term) {
-      params.set("page", "1");
       params.set("query", term);
     } else {
       params.delete("query");
